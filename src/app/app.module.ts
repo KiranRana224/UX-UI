7e306f972c9bffb77b29be4036c7407e9545dad4
Kiran <ranakiran2403@gmail.com>
2025-03-11 22:34:41 +0530

    Reapply "test"
    This reverts commit 686e1634c004e5b125cbe060525328eac53760ab.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CardComponent } from './card/card.component';
-
+import { SelectDropdownComponent } from './UX-UI/select-dropdown/select-dropdown.component';
+import { materialModule } from 'src/material.module';
+import { AutocompleteComponent } from './UX-UI/autocomplete/autocomplete.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
+import { MatOptionModule } from '@angular/material/core';
 @NgModule({
-  declarations: [AppComponent, CardComponent],
-  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+  declarations: [AppComponent, SelectDropdownComponent, AutocompleteComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    materialModule,
+    HttpClientModule,
+    MatAutocompleteModule,
+    MatInputModule,
+    MatOptionModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
