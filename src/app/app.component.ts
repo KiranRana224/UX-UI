0df4d7bbabdcc468769003ebf35581c7a3d0827e
Kiran <ranakiran2403@gmail.com>
2025-03-16 16:23:43 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('slideIn', [
+      state(
+        'hidden',
+        style({
+          transform: 'translateX(-100%)',
+          opacity: 0,
+        })
+      ),
+      state(
+        'visible',
+        style({
+          transform: 'translateX(0)',
+          opacity: 1,
+        })
+      ),
+      transition('hidden => visible', [animate('500ms ease-in')]),
+      transition('visible => hidden', [animate('500ms ease-out')]),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'AngularUI';
+  state = 'hidden';
+
+  toggleSlide() {
+    this.state = this.state === 'hidden' ? 'visible' : 'hidden';
+  }
 }
